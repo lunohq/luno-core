202913b12b8871e0ff5ba57d7a94e993f986791f
Michael Hahn <mwhahn@gmail.com>
2016-04-08 00:17:55 -0700

    Add getAnswers

--- src/db/answer.js
+++ src/db/answer.js
@@ -6,6 +6,10 @@ const table = resolveTableName('answer');
 
 export class Answer extends Object {};
 
+function getCompositeId(teamId, botId) {
+  return `${teamId}=${botId}`;
+}
+
 export function createAnswer(answer) {
   return new Promise((resolve, reject) => {
     answer.id = uuid.v4();
@@ -22,7 +26,7 @@ export function createAnswer(answer) {
 }
 
 export function getAnswer(teamId, botId, id) {
-  const compositeId = `${teamId}-${botId}`;
+  const compositeId = getCompositeId(teamId, botId);
   return new Promise((resolve, reject) => {
     const params = {
       TableName: table,
@@ -33,6 +37,24 @@ export function getAnswer(teamId, botId, id) {
       if (err) return reject(err);
       const answer = fromDB(Answer, data.Item);
       return resolve(answer);
+    });
+  });
+}
+
+export function getAnswers(teamId, botId) {
+  const compositeId = getCompositeId(teamId, botId);
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: table,
+      KeyConditionExpression: 'HashKey = :hkey',
+      ExpressionAttributeValues: {
+        ':hkey': compositeId,
+      },
+    };
+
+    client.query(params, (err, data) => {
+      if (err) return reject(err);
+      return data.Items.map((item) => fromDB(Answer, item));
     });
   });
 }
