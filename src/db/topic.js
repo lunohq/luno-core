f2f5851369143038db80574f57e7cc86f686bbd1
Michael Hahn <mwhahn@gmail.com>
2016-06-29 16:01:19 -0700

    Put won't return a response

--- src/db/topic.js
+++ src/db/topic.js
@@ -12,10 +12,13 @@ export const DUPLICATE_TOPIC_NAME_EXCEPTION = 'DuplicateTopicNameException'
 export class Topic {}
 
 export async function createTopic({ teamId, ...data }) {
-  if (!data.isDefault && !isValidName({ teamId, name: data.name })) {
-    const error = new Error('Duplicate team name')
-    error.code = DUPLICATE_TOPIC_NAME_EXCEPTION
-    throw error
+  if (!data.isDefault) {
+    const validName = await isValidName({ teamId, name: data.name })
+    if (!validName) {
+      const error = new Error('Duplicate team name')
+      error.code = DUPLICATE_TOPIC_NAME_EXCEPTION
+      throw error
+    }
   }
 
   const topic = new Topic()
@@ -147,14 +150,14 @@ export async function isValidName({ teamId, name }) {
   }
 
   try {
-    const resp = await client.put(params).promise()
-    return resp.statusCode === 200
+    await client.put(params).promise()
   } catch (err) {
     if (err.code === 'ConditionalCheckFailedException') {
       return false
     }
     throw err
   }
+  return true
 }
 
 export async function getTopic({ teamId, id }) {
